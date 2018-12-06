0cad8099543fbc5ddeb2bf21ebad9c1b43256758
Mitch Patenaude <patenaude@gmail.com>
2018-12-06 13:26:37 -0800

    quick-n-dirty script to make daily messages for the website

---
+++ make_daily_messages.sh
@@ -0,0 +1,5 @@
+./babble.py -C -m 14 -c 20 -j stores/sharon.sqlite > html/char14.json
+./babble.py -C -m 8 -c 20 -j stores/sharon.sqlite > html/char8.json
+./babble.py -m 4 -c 20 -j stores/sharon.sqlite > html/word4.json
+./babble.py -m 3 -c 20 -j stores/sharon.sqlite > html/word3.json
+
