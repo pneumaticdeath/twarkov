da7b558922efc397da90e77ca8147255405a447f
Mitch Patenaude <patenaude@gmail.com>
2018-12-13 13:28:21 -0800

    Switched to 15 character chains and retired 8 character chains

--- make_daily_messages.sh
+++ make_daily_messages.sh
@@ -1,8 +1,5 @@
-echo "14 character chains:"
-time ./babble.py -d -C -m 14 -c 20 -j stores/sharon.sqlite > html/char14.json
-
-echo "8 character chains:"
-time ./babble.py -d -C -m 8 -c 20 -j stores/sharon.sqlite > html/char8.json
+echo "15 character chains:"
+time ./babble.py -d -C -m 15 -c 20 -j stores/sharon.sqlite > html/char15.json
 
 echo "4 word chains:"
 time ./babble.py -d -m 4 -c 20 -j stores/sharon.sqlite > html/word4.json
