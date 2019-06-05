cdb5169c2faaac32b62380e6c3b63fc290157343
Mitch Patenaude <patenaude@gmail.com>
2019-06-05 11:03:52 -0700

    Remvoing 3 word and 15 character chains... since they almost never produce interesting output

--- make_daily_messages.sh
+++ make_daily_messages.sh
@@ -3,12 +3,12 @@
 echo "18 character chains:"
 time ./babble.py -s -d -C -m 18 -c 20 -j stores/sharon.sqlite > html/char18.json
 
-echo "15 character chains:"
-time ./babble.py -s -d -C -m 15 -c 20 -j stores/sharon.sqlite > html/char15.json
+# echo "15 character chains:"
+# time ./babble.py -s -d -C -m 15 -c 20 -j stores/sharon.sqlite > html/char15.json
 
 echo "4 word chains:"
 time ./babble.py -s -d -m 4 -c 20 -j stores/sharon.sqlite > html/word4.json
 
-echo "3 word chains:"
-time ./babble.py -s -d -m 3 -c 20 -j stores/sharon.sqlite > html/word3.json
+# echo "3 word chains:"
+# time ./babble.py -s -d -m 3 -c 20 -j stores/sharon.sqlite > html/word3.json
 
