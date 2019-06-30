c8b9a05982accfc3f83abd911fdb2a93760d39cc
Mitch Patenaude <patenaude@gmail.com>
2019-06-29 17:52:11 -0700

    Another step in decoupling chain from store

--- users.py
+++ users.py
@@ -4,6 +4,7 @@ import json
 import os
 import twitter
 import twarkov
+import tweetdb
 
 
 CONFIG_PATH = '~/.twarkov'
@@ -13,6 +14,7 @@ class User(object):
         self._config = config
         self._api = None
         self._chain = None
+        self._store = None
 
     @property
     def api(self):
@@ -25,6 +27,15 @@ class User(object):
         if self._chain is None:
             self._chain = twarkov.TwarkovChain(api=self.api, **self._config['chain'])
         return self._chain
+
+    @property
+    def tweetstore(self):
+        if self._store is None:
+            if '.sqlite' in self._config['tweetstore']:
+                self._store = tweetdb.TweetStoreSQL(self._config['tweetstore'])
+            else:
+                self._store = tweetdb.TweetStore(self._config['tweetstore'])
+        return self._store
 
 def load(username):
     configfile = os.path.join(CONFIG_PATH, 'users.json')
