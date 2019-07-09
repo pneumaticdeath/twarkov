41dda74d1f59b3711dcf532be2a924925ffb5a49
Mitch Patenaude <patenaude@gmail.com>
2019-07-09 13:40:24 -0700

    Config path created if it doesn't exist

--- config.py
+++ config.py
@@ -7,7 +7,13 @@ import os
 CONFIG_DIR = '~/.twarkov'
 
 def get_path(config_file):
-    return os.path.join(os.path.expanduser(CONFIG_DIR), config_file)
+    newpath = os.path.join(os.path.expanduser(CONFIG_DIR), config_file)
+    try:
+        os.makedirs(os.path.dirname(newpath), mode=0700)
+    except OSError as e:
+        if 'exists' not in str(e):
+            raise e
+    return newpath
 
 def get_file(config_file, mode='r'):
     return open(get_path(config_file), mode)
