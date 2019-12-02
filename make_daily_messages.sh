ecd4d1c093be77cd927ee5cb0cb95b54509d7f86
Mitch Patenaude <patenaude@gmail.com>
2019-12-02 10:33:34 -0800

    Moved to smaller number of messages generated hourly

--- make_daily_messages.sh
+++ make_daily_messages.sh
@@ -4,16 +4,16 @@ export PYTHONPATH=/home/mitch/pylib
 cd $(dirname $0)
 
 echo "20 character chains:"
-time ./babble.py -s -d -C -m 20 -c 20 -j stores/sharon.sqlite > html/char20.json
+time ./babble.py -s -d -C -m 20 -c 5 -j stores/sharon.sqlite > html/char20.json
 
 echo "18 character chains:"
-time ./babble.py -s -d -C -m 18 -c 20 -j stores/sharon.sqlite > html/char18.json
+time ./babble.py -s -d -C -m 18 -c 5 -j stores/sharon.sqlite > html/char18.json
 
 # echo "15 character chains:"
 # time ./babble.py -s -d -C -m 15 -c 20 -j stores/sharon.sqlite > html/char15.json
 
 echo "4 word chains:"
-time ./babble.py -s -d -m 4 -c 20 -j stores/sharon.sqlite > html/word4.json
+time ./babble.py -s -d -m 4 -c 5 -j stores/sharon.sqlite > html/word4.json
 
 # echo "3 word chains:"
 # time ./babble.py -s -d -m 3 -c 20 -j stores/sharon.sqlite > html/word3.json
