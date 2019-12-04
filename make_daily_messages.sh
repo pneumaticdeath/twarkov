e3e8d4274556e2ac409e1caf43080840bf2fa8e4
Mitch Patenaude <patenaude@gmail.com>
2019-12-04 11:35:17 -0800

    Created archive of hourly messages

--- make_daily_messages.sh
+++ make_daily_messages.sh
@@ -2,20 +2,20 @@
 
 export PYTHONPATH=/home/mitch/pylib
 cd $(dirname $0)
+ds=$(date +%Y-%m-%d-%H%M)
+archivedir=json_archive
+htmldir=html
 
 echo "20 character chains:"
-time ./babble.py -s -d -C -m 20 -c 5 -j stores/sharon.sqlite > html/char20.json
+time ./babble.py -s -d -C -m 20 -c 5 -j stores/sharon.sqlite > ${archivedir}/char20-${ds}.json
+ln -f ${archivedir}/char20-${ds}.json ${htmldir}/char20.json
 
 echo "18 character chains:"
-time ./babble.py -s -d -C -m 18 -c 5 -j stores/sharon.sqlite > html/char18.json
-
-# echo "15 character chains:"
-# time ./babble.py -s -d -C -m 15 -c 20 -j stores/sharon.sqlite > html/char15.json
+time ./babble.py -s -d -C -m 18 -c 5 -j stores/sharon.sqlite > ${archivedir}/char18-${ds}.json
+ln -f ${archivedir}/char18-${ds}.json ${htmldir}/char18.json
 
 echo "4 word chains:"
-time ./babble.py -s -d -m 4 -c 5 -j stores/sharon.sqlite > html/word4.json
-
-# echo "3 word chains:"
-# time ./babble.py -s -d -m 3 -c 20 -j stores/sharon.sqlite > html/word3.json
+time ./babble.py -s -d -m 4 -c 5 -j stores/sharon.sqlite > ${archivedir}/word4-${ds}.json
+ln -f ${archivedir}/word4-${ds}.json ${htmldir}/word4.json
 
 ./sync.sh
