50105cbe036d9877890551719bd2e8ba39e154e5
Mitch Patenaude <patenaude@gmail.com>
2020-02-21 09:06:54 -0800

    Checking in posts, put datestamps in message generation

--- make_messages.sh
+++ make_messages.sh
@@ -6,15 +6,15 @@ ds=$(date +%Y-%m-%d-%H%M)
 archivedir=json_archive
 htmldir=html
 
-echo "20 character chains:"
+echo "$(date): 20 character chains:"
 time ./babble.py -s -d -C -m 20 -c 5 -j stores/sharon.sqlite > ${archivedir}/char20-${ds}.json
 ln -f ${archivedir}/char20-${ds}.json ${htmldir}/char20.json
 
-echo "18 character chains:"
+echo "$(date): 18 character chains:"
 time ./babble.py -s -d -C -m 18 -c 5 -j stores/sharon.sqlite > ${archivedir}/char18-${ds}.json
 ln -f ${archivedir}/char18-${ds}.json ${htmldir}/char18.json
 
-echo "4 word chains:"
+echo "$(date): 4 word chains:"
 time ./babble.py -s -d -m 4 -c 10 -j stores/sharon.sqlite > ${archivedir}/word4-${ds}.json
 ln -f ${archivedir}/word4-${ds}.json ${htmldir}/word4.json
 
