57338a84845d1506dd1ff7a0a5be6934866e9a20
Mitch Patenaude <patenaude@gmail.com>
2020-04-28 10:48:43 -0700

    generalized update code

---
+++ update.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+
+# Copyright 2009, 2020, Mitch Patenaude
+
+import argparse
+import sys
+import users
+
+from chain_utils import *
+
+if __name__ == "__main__":
+  
+    parser = argparse.ArgumentParser('update.py')
+    parser.add_argument('--continuous', '-c', action='store_true', help='Update continuously')
+    parser.add_argument('--user', '-u', default='sharon', help='Which user to update')
+    args = parser.parse_args()
+
+    user = users.load(args.user)
+    if args.continuous:
+        continuous_update(user)
+    else:
+        check_updates(user)
+
+
